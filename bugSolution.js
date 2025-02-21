248c3974d51792976309ef4f7844ed8a048c327b
BugHunter-X <askzenai@gmail.com>
2025-02-21 06:56:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,11 @@
+```javascript
+//Correct usage of $inc operator in MongoDB aggregation pipeline
+db.collection.aggregate([
+  {
+    $group: {
+      _id: "$fieldName",
+      count: { $sum: 1 }
+    }
+  }
+])
+```
